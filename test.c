eaac5df9bad406a9243fc611ea44dd20fc54b366
Fredrik Nygell <fnl18001@student.mdh.se>
2022-05-09 14:21:26 +0200

    Some comments that are useless but we will see what will happen

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 int main(){
     printf("This is a test in which we would like to see what git overwrites and what it doesn't.\n");
     printf("What happens when I remove your code and replace it with mine?\n");
+    /*Some comments that are meaningless and this is taking way to long... I do not feel terribly bright.*/
     return 0;
 }
 
