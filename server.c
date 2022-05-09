deb9b75c6ef03f042ee0cc1819e17af69b3d0848
Fredrik Nygell <fnl18001@student.mdh.se>
2022-05-09 15:08:23 +0200

    Added some packages and created a main()

--- server.c
+++ server.c
@@ -9,11 +9,18 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 #define PORT 5555
 #define hostNameLength 50
 #define messageLength 256
 #define MAXMSG 512
+
+int main(){
+
+    
+    return 0;
+}
 
 
 
