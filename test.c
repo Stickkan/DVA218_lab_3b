b5078d2bdc1bdf6a3c758eb2314277a6c2cea444
Fredrik Nygell <fnl18001@student.mdh.se>
2022-05-09 13:22:28 +0200

    A test file

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+int main(){
+    printf("This is a test in which we would like to see what git overwrites and what it doesn't.\n");
+    return 0;
+}
