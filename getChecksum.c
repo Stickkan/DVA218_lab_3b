0b9aa37005d971d6337fef799c101b3e46243278
Viktor <vp.96@hotmail.com>
2022-05-18 00:53:44 +0200

    Maybe done? Error code seems to work. Needs double checking in the morning..

---
+++ getChecksum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "header.h"
+
+int getChecksum(const char* string){
+ 
+    int sum = 0;
+    
+    for(int i = 0; i<strlen(string); i++){
+        sum += (int)string[i];                  /*This is the entire sum of all characters in the string added*/
+    }
+
+    int checksum = (255 - (sum % 255));         /*The checksum cannot be greater than 2^8 = 256 */
+
+    return checksum;
+}
